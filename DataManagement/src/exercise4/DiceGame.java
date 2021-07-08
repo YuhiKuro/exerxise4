74a34379f8847a0ca6d1997150e2b5d8d1a92368
黒鵜雄飛 <kurou_yuhi@plise.co.jp>
2021-07-09 06:34:44 +0900

    Won/lost message modified

--- DataManagement/src/exercise4/DiceGame.java
+++ DataManagement/src/exercise4/DiceGame.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 
 public class DiceGame {
 
-	String name;
+	static String name;
+	static int totalVal;
 
 	public static void main(String[] args) {
 		DiceGame diceGame = new DiceGame();
 		diceGame.greet();
 		diceGame.showDice();
+		diceGame.judge(name, totalVal);
 	}
 	public void showDice() {
 		Random rand = new Random();
 		int diceNum1 = rand.nextInt(6) + 1;
 		int diceNum2 = rand.nextInt(6) + 1;
-		int totalVal = diceNum1 + diceNum2;
+		totalVal = diceNum1 + diceNum2;
 
 		System.out.println("Rolling the dice...");
 		System.out.println("Die 1 : " + diceNum1);
@@ -30,6 +32,14 @@ public class DiceGame {
 		Scanner sc = new Scanner(System.in);
 		name = sc.next();
 		System.out.println("Hello, " + name + "!");
+	}
+
+	public void judge(String name, int value) {
+		if (value>7) {
+			System.out.println(name + " won!");
+		} else {
+			System.out.println(name + " lose!");
+		}
 	}
 
 }

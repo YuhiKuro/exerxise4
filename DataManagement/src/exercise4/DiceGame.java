a47fa90d06cccf5720592b2797d4cf22cff89d86
黒鵜雄飛 <kurou_yuhi@plise.co.jp>
2021-07-09 06:28:52 +0900

    Greetings added

--- DataManagement/src/exercise4/DiceGame.java
+++ DataManagement/src/exercise4/DiceGame.java
@@ -1,11 +1,15 @@
 package exercise4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
 
+	String name;
+
 	public static void main(String[] args) {
 		DiceGame diceGame = new DiceGame();
+		diceGame.greet();
 		diceGame.showDice();
 	}
 	public void showDice() {
@@ -18,6 +22,14 @@ public class DiceGame {
 		System.out.println("Die 1 : " + diceNum1);
 		System.out.println("Die 2 : " + diceNum2);
 		System.out.println("Total value : " + totalVal);
+	}
+
+	public void greet() {
+		System.out.println("What your name?");
+		System.out.print("> ");
+		Scanner sc = new Scanner(System.in);
+		name = sc.next();
+		System.out.println("Hello, " + name + "!");
 	}
 
 }

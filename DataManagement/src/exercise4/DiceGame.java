cb6ec0e383e487f704ff0967ad2f0d7951acd954
黒鵜雄飛 <kurou_yuhi@plise.co.jp>
2021-07-09 06:17:55 +0900

    Initial revision

---
+++ DataManagement/src/exercise4/DiceGame.java
@@ -0,0 +1,23 @@
+package exercise4;
+
+import java.util.Random;
+
+public class DiceGame {
+
+	public static void main(String[] args) {
+		DiceGame diceGame = new DiceGame();
+		diceGame.showDice();
+	}
+	public void showDice() {
+		Random rand = new Random();
+		int diceNum1 = rand.nextInt(6) + 1;
+		int diceNum2 = rand.nextInt(6) + 1;
+		int totalVal = diceNum1 + diceNum2;
+
+		System.out.println("Rolling the dice...");
+		System.out.println("Die 1 : " + diceNum1);
+		System.out.println("Die 2 : " + diceNum2);
+		System.out.println("Total value : " + totalVal);
+	}
+
+}
